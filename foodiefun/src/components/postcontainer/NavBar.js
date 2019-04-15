b68f5be26fc951f892ab6c7744d7dad371a063a5
ntsum <natsukajiwaram@gmail.com>
2019-04-15 17:17:04 -0400

    simple nav

---
+++ foodiefun/src/components/postcontainer/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+class NavBar extends React.Component {
+  render() {
+    return (
+      <div className="navBar">
+        FoodieFun 
+        <input placeholder="Search" name="Search" />
+      </div>
+    );
+  }
+}
+
+export default NavBar;
--- foodiefun/src/components/postcontainer/PostPage.js
+++ foodiefun/src/components/postcontainer/PostPage.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import dummyData from "../../dummy-data";
 import AccountMenu from "./AccountMenu/AccountMenu";
 import RestaurantList from './RestaurantList';
+import NavBar from './NavBar'
 
 export default class PostPage extends Component {
   constructor() {
@@ -14,16 +15,14 @@ export default class PostPage extends Component {
 
   componentDidMount() {
     this.setState({
-      data:
-        localStorage.getItem("data") === null
-          ? dummyData
-          
+      data:dummyData  
     });
   }
 
   render() {
       return (
         <div>
+          <NavBar/>
           <AccountMenu account={this.state.account} />
           <div>
             <RestaurantList data={this.state.data}/>
