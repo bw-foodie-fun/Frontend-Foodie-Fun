c7afa6912500b64b07f9cc1771a84364427efea9
Mario Amaya <thefozcat@gmail.com>
2019-04-15 11:57:19 -0700

    created restaurant page route

---
+++ foodiefun/src/components/postcontainer/RestaurantPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RestaurantPage = props => {
+    return (
+        <div>
+            <h1>RestaurantPage</h1>
+        </div>
+    )
+}
+
+export default RestaurantPage;
