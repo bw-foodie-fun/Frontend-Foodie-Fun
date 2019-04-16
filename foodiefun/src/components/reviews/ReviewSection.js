63ea72b03205f07825fc88039841d687f31b0676
Mario Amaya <thefozcat@gmail.com>
2019-04-16 09:32:46 -0700

    updated state tree

--- foodiefun/src/components/reviews/ReviewSection.js
+++ foodiefun/src/components/reviews/ReviewSection.js
@@ -1,51 +1,51 @@
-import React from "react";
-import Review from "./Reviews";
+// import React from "react";
+// import Review from "./Reviews";
 
-class ReviewSection extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      reviewData: props.ReviewData,
-      inputText: ""
-    };
-  }
-  handleChange = event => {
-    this.setState({ inputText: event.target.value });
-  };
+// class ReviewSection extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       reviewData: props.ReviewData,
+//       inputText: ""
+//     };
+//   }
+//   handleChange = event => {
+//     this.setState({ inputText: event.target.value });
+//   };
 
-  handleSubmit = event => {
-    event.preventDefault();
-    //first make a copy of the state
-    const stateCopy = this.state.reviewData.slice();
-    //second create the new review
-    const newReview = {
-      username: "",
-      text: this.state.inputText
-    };
-    stateCopy.push(newReview);
-    //third update the state withj setState
-    this.setState({
-      reviewData: stateCopy
-    });
-  };
-  render() {
-    return (
-      <div className="reviews">
-        {this.state.reviewData.map((review, index) => (
-          <Review key={index} reviewData={review} />
-        ))}
-        <form action="" onSubmit={this.handleSubmit}>
-          <input
-            className="reviewForm"
-            type="text"
-            onChange={this.handleChange}
-            value={this.state.inputText}
-            placeholder="Add a Review"
-          />
-        </form>
-      </div>
-    );
-  }
-}
+//   handleSubmit = event => {
+//     event.preventDefault();
+//     //first make a copy of the state
+//     const stateCopy = this.state.reviewData.slice();
+//     //second create the new review
+//     const newReview = {
+//       username: "",
+//       text: this.state.inputText
+//     };
+//     stateCopy.push(newReview);
+//     //third update the state withj setState
+//     this.setState({
+//       reviewData: stateCopy
+//     });
+//   };
+//   render() {
+//     return (
+//       <div className="reviews">
+//         {this.state.reviewData.map((review, index) => (
+//           <Review key={index} reviewData={review} />
+//         ))}
+//         <form action="" onSubmit={this.handleSubmit}>
+//           <input
+//             className="reviewForm"
+//             type="text"
+//             onChange={this.handleChange}
+//             value={this.state.inputText}
+//             placeholder="Add a Review"
+//           />
+//         </form>
+//       </div>
+//     );
+//   }
+// }
 
-export default ReviewSection;
+// export default ReviewSection;
