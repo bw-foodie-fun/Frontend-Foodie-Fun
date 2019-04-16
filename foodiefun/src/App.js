6fb0ca842f0be2e89bb0316d7280d23f170ee3bc
ntsum <natsukajiwaram@gmail.com>
2019-04-16 13:39:56 -0400

    moved state off of ReviewSection, just have to make it functional now

--- foodiefun/src/App.js
+++ foodiefun/src/App.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
-import {Route} from 'react-router-dom';
-import axios from 'axios';
-import PostPage from './components/postcontainer/PostPage';
-import RestaurantPage from './components/postcontainer/RestaurantPage';
-import Login from './components/login/Login'
-import './App.css';
-import withAuthenticate from './components/authenticate/withAuthenticate';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import axios from "axios";
+import PostPage from "./components/postcontainer/PostPage";
+import RestaurantPage from "./components/postcontainer/RestaurantPage";
+import Login from "./components/login/Login";
+import "./App.css";
+import withAuthenticate from "./components/authenticate/withAuthenticate";
 import NewLogin from "./components/login/NewLogin";
 
 const Auth = withAuthenticate(Login)(PostPage);
@@ -15,44 +15,64 @@ class App extends Component {
     super();
     this.state = {
       account: true,
-      data: []
-    }
+      data: [],
+      reviewData: "",
+      inputText: ""
+    };
   }
 
+  handleChange = event => {
+    this.setState({ inputText: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    //first make a copy of the state
+    const stateCopy = this.state.reviewData.slice();
+    //second create the new review
+    const newReview = {
+      username: "",
+      text: this.state.inputText
+    };
+    stateCopy.push(newReview);
+    //third update the state with setState
+    this.setState({
+      reviewData: stateCopy
+    });
+  }; 
+
   componentDidMount() {
-    axios.get('https://foodie-fun.herokuapp.com/api/meals/all')
+    axios
+      .get("https://foodie-fun.herokuapp.com/api/meals/all")
       .then(res => {
-        this.setState({data: res.data})
+        this.setState({ data: res.data });
       })
       .catch(err => {
-        alert(err)
-      })
+        alert(err);
+      });
   }
 
   render() {
     return (
       <div>
         <Route
-          exact path="/"
-          render={props => 
+          exact
+          path="/"
+          render={props => (
             <Auth
               {...props}
               data={this.state.data}
               account={this.state.account}
             />
-          }
+          )}
         />
         <Route exact path="/newlogin" component={NewLogin} />
         <Route
           path="/restaurant/:id"
-          render={props => 
-            <RestaurantPage
-              {...props}
-              data={this.state.data}
-            />
-          }/>
+          render={props => <RestaurantPage {...props} data={this.state.data} />}
+        />
       </div>
-    )
+    );
   }
 }
 
